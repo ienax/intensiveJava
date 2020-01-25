bb671cd859152191e2dab0c5b54b552e48183679
ienax <postnikov.nikolai@gmail.com>
2020-01-25 12:13:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import skilllbox_java.My_Thread;
+
+import java.awt.*;
+import java.io.*;
+
+
+
+public class Main {
+    public static void main(String[] args) throws AWTException, IOException {
+        My_Thread thread = new My_Thread();
+        thread.start();
+    }
+}
